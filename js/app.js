5fb3db3c02845bffcdf665470d6ce197d7840b10
javiermelendrez1 <81501034+javiermelendrez1@users.noreply.github.com>
2021-07-01 14:24:46 -0700

    all data from api is retrieved

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 console.log('app.js is connected');
 //query for the input form 
 const theForm = document.querySelector('form');
@@ -17,8 +18,36 @@ theForm.addEventListener('submit', (e) => {
 const getApi = async (ticker) => {
     //await for the response of the api
     const response  = await axios.get(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=GJDAGA8VL2CMHAOX`);
-
- 
+    //await for the second response
+    //we need a second response because it is two different api fetches 
+    const response2 = await axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=GJDAGA8VL2CMHAOX`);
     //print to console log the response
-    console.log(response);
+    console.log(response2.data["Global Quote"]["05. price"]); 
+    console.log(response2.data["Global Quote"]["09. change"]); 
+    console.log(response2.data["Global Quote"]["10. change percent"]); 
+    console.log(response.data.Name);
+    console.log(response.data.Symbol);
+    console.log(response.data.Description);
+    console.log(response.data.MarketCapitalization); 
+    console.log(response.data.PERatio); 
+    console.log(response.data.DividendYield);
+
+    //now save the data into variables 
+    const stockName = response.data.Name;
+    const stockSymbol = response.data.Symbol;
+    const stockPrice = response2.data["Global Quote"]["05. price"];
+    const stockPriceChange = response2.data["Global Quote"]["09. change"];
+    const stockPriceChangePercent = response2.data["Global Quote"]["10. change percent"];
+    const stockDescription = response.data.Description;
+    const stockMarketCap = response.data.MarketCapitalization;
+    const stockPriceEarningsRatio = response.data.PERatio;
+    const stockDividendYield = response.data.DividendYield;
+
+    //now make a function that will dislay all of this information on screen 
+    //pass in all of these variables 
+    
+}
+const printData = (name, symbol, price, priceChange, priceChangePercent, marketCap, priceEarningsRatio, dividendYield) => {
+    
+
 }

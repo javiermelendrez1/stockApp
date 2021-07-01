a0017b54bfb87d762476c32ef8864b96e7c46a86
javiermelendrez1 <81501034+javiermelendrez1@users.noreply.github.com>
2021-07-01 14:59:51 -0700

    all data now gets printed

--- js/app.js
+++ js/app.js
@@ -14,22 +14,22 @@ theForm.addEventListener('submit', (e) => {
     getApi(userInput);
 
 })
-//create async function
+//create async function to fetch api and its data
 const getApi = async (ticker) => {
     //await for the response of the api
-    const response  = await axios.get(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=GJDAGA8VL2CMHAOX`);
+    const response = await axios.get(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=GJDAGA8VL2CMHAOX`);
     //await for the second response
     //we need a second response because it is two different api fetches 
     const response2 = await axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=GJDAGA8VL2CMHAOX`);
     //print to console log the response
-    console.log(response2.data["Global Quote"]["05. price"]); 
-    console.log(response2.data["Global Quote"]["09. change"]); 
-    console.log(response2.data["Global Quote"]["10. change percent"]); 
+    console.log(response2.data["Global Quote"]["05. price"]);
+    console.log(response2.data["Global Quote"]["09. change"]);
+    console.log(response2.data["Global Quote"]["10. change percent"]);
     console.log(response.data.Name);
     console.log(response.data.Symbol);
     console.log(response.data.Description);
-    console.log(response.data.MarketCapitalization); 
-    console.log(response.data.PERatio); 
+    console.log(response.data.MarketCapitalization);
+    console.log(response.data.PERatio);
     console.log(response.data.DividendYield);
 
     //now save the data into variables 
@@ -45,9 +45,30 @@ const getApi = async (ticker) => {
 
     //now make a function that will dislay all of this information on screen 
     //pass in all of these variables 
-    
+    printData(stockName, stockSymbol, stockPrice, stockPriceChange, stockPriceChangePercent, stockDescription, stockMarketCap, stockPriceEarningsRatio, stockDividendYield);
 }
-const printData = (name, symbol, price, priceChange, priceChangePercent, marketCap, priceEarningsRatio, dividendYield) => {
-    
+//query for div that will hold ticker info
+const tickerInfoDiv = document.querySelector('#tickerInfo');
+
+//create function to print data pass in all data from api fetch
+const printData = (name, symbol, price, priceChange, priceChangePercent, description, marketCap, priceEarningsRatio, dividendYield) => {
+    //set the inner html equal to the dagta variable
+    let data = `    <div class="container" id="tickerInfo">
+    <span class="stockName">${name}</span><span class="tickerSymbol">(${symbol})<br></span>
+    <span class="stockPrice">$${price}</span><span class="stockPriceChange"> ${priceChange}</span><span
+        class="stockPricePercentChange">(${priceChangePercent})<br></span>
+    <span>About<br></span>
+    <span class="companyDescription">
+    ${description}<br>
+    </span>
+    <span>Market Capitalization<br></span>
+    <span class="marketCap">${marketCap}<br></span>
+    <span>Price Earnings Ratio<br></span>
+    <span class="priceEarningsRatio">${priceEarningsRatio}<br></span>
+    <span>Dividend Yeild<br></span>
+    <span class="dividendYield">${dividendYield}</span>
+</div>`;
+
+    tickerInfoDiv.innerHTML = data;
 
 }
